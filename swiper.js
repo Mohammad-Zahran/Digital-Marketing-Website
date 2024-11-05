b46b5cb07e3e5402f99b0761a10a8a4f50e46d41
Mohammad Zahran <125310252+AbouZik@users.noreply.github.com>
2024-11-05 03:29:16 +0200

    Commiting in case of a bug

--- swiper.js
+++ swiper.js
@@ -1,31 +1,30 @@
-new Swiper('.card-wrapper', {
+let swiper = new Swiper('.card-wrapper', {
     loop: true,
     spaceBetween: 30,
   
     // Pagination bullets
     pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-      dynamicBullets: true
+        el: '.swiper-pagination',
+        clickable: true,
+        dynamicBullets: true,
     },
   
     // Navigation arrows
     navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
     },
 
     // Responsive breakpoints
     breakpoints: {
         0: {
-            slidersPerView: 1
+            slidesPerView: 1,  // 1 card for screens smaller than 768px
         },
         768: {
-            slidersPerView: 2
+            slidesPerView: 2,  // 2 cards for screens larger than 768px
         },
         1024: {
-            slidersPerView: 3
+            slidesPerView: 3,  // 3 cards for screens larger than 1024px
         },
-    }
-
-  });
+    },
+});
